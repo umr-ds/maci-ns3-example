475d35ed339ffad4966e222a09949ace07049854
maci-auto-committer <auto-committer@maci-research.net>
2018-08-02 11:45:53 +0000

    create/update experimentTemplate

--- script.py
+++ script.py
@@ -1,90 +1,109 @@
-### ENV int bandwidth "BW"
-### ENV int delay "Delay"
-### ENV int packetSize "Packet Size"
-### ENV int maxPacketCount "Count"
-### ENV float interval "Interval"
-
-# Network topology
-# 
-#       n0    n1   n2   n3
-#       |     |    |    |
-#       =================
-#              LAN
-#
-# - UDP flows from n0 to n1 and back
-# - DropTail queues 
-# - Tracing of queues and packet receptions to file "udp-echo-py.tr"
-
+import ns3
 import framework
-import sys
 
-import ns.core
-import ns.csma
-import ns.applications
-import ns.internet
-import ns.netanim
-
-if __name__ == '__main__':
+def main(argv):
 
     framework.start()
-    
-    #ns.core.LogComponentEnable("UdpEchoClientApplication", ns.core.LOG_LEVEL_ALL)
-    #ns.core.LogComponentEnable("UdpEchoServerApplication", ns.core.LOG_LEVEL_ALL)
-    
-    cmd = ns.core.CommandLine()
-    cmd.useIpv6 = "False"
-    cmd.AddValue("useIpv6", "Use Ipv6")
-    cmd.Parse(sys.argv)
-    
-    print "Create nodes."
-    n = ns.network.NodeContainer()
-    n.Create(4)
-	
-    internetstack = ns.internet.InternetStackHelper()
-    internetstack.Install(n)
-    
-    print "Create channels."
-    csma = ns.csma.CsmaHelper()
-    csma.SetChannelAttribute("DataRate", ns.core.StringValue(str(framework.param('bandwidth'))))
-    csma.SetChannelAttribute("Delay", ns.core.TimeValue(ns.core.MilliSeconds({{delay}})))
-    csma.SetDeviceAttribute("Mtu", ns.core.UintegerValue(1400))
-    d = csma.Install(n)
-    
-    print "Assign IP Addresses."
-    ipv4 = ns.internet.Ipv4AddressHelper()
-    ipv4.SetBase (ns.network.Ipv4Address("10.1.1.0"), ns.network.Ipv4Mask("255.255.255.0"))
-    i = ipv4.Assign(d)
-    serverAddress = ns.network.Address(i.GetAddress(1))
-    
-    print "Create Applications."
-    port = 9
-    server = ns.applications.UdpEchoServerHelper(port)
-    serverapps = server.Install(n.Get(1))
-    serverapps.Start(ns.core.Seconds(1.0))
-    serverapps.Stop(ns.core.Seconds(10.0))
-    
-    interPacketInterval = ns.core.Seconds({{interval}})
-    
-    client = ns.applications.UdpEchoClientHelper(serverAddress, port)
-    client.SetAttribute("MaxPackets", ns.core.UintegerValue({{maxPacketCount}}))
-    client.SetAttribute("Interval", ns.core.TimeValue(interPacketInterval))
-    client.SetAttribute("PacketSize", ns.core.UintegerValue({{packetSize}}))
-    
-    apps = client.Install(n.Get(0))
-    apps.Start(ns.core.Seconds(2.0))
-    apps.Stop(ns.core.Seconds(10.0))
-    
-    client.SetFill (apps.Get (0), "Hello World")
-    client.SetFill (apps.Get (0), 0xa5, 1024)
-    
-    asciitracer = ns.network.AsciiTraceHelper()
-    csma.EnableAsciiAll(asciitracer.CreateFileStream("udp-echo-py.tr"))
-    csma.EnablePcapAll("udp-echo-py", False)
-    
-    print "Run Simulation."
-    anim = ns.animation.AnimationInterface("animation.xml")
-    ns.core.Simulator.Run()
-    ns.core.Simulator.Destroy()
-    print "Done."
-    
+
+    cmd = ns3.CommandLine()
+    cmd.Parse (argv)
+
+    # Configuration.
+
+    # Build nodes
+    term_0 = ns3.NodeContainer()
+    term_0.Create (1)
+    term_1 = ns3.NodeContainer()
+    term_1.Create (1)
+    bridge_0 = ns3.NodeContainer()
+    bridge_0.Create (1)
+    bridge_1 = ns3.NodeContainer()
+    bridge_1.Create (1)
+
+    # Build link.
+    csma_bridge_0 = ns3.CsmaHelper();
+    csma_bridge_0.SetChannelAttribute("DataRate", ns3.DataRateValue (ns3.DataRate(100000000)))
+    csma_bridge_0.SetChannelAttribute("Delay",  ns3.TimeValue (ns3.MilliSeconds(10000)))
+    csma_bridge_1 = ns3.CsmaHelper();
+    csma_bridge_1.SetChannelAttribute("DataRate", ns3.DataRateValue (ns3.DataRate(100000000)))
+    csma_bridge_1.SetChannelAttribute("Delay",  ns3.TimeValue (ns3.MilliSeconds(10000)))
+    csma_hub_0 = ns3.CsmaHelper()
+    csma_hub_0.SetChannelAttribute("DataRate", ns3.DataRateValue(ns3.DataRate(100000000)))
+    csma_hub_0.SetChannelAttribute("Delay",  ns3.TimeValue(ns3.MilliSeconds(10000)))
+
+    # Build link net device container.
+    all_bridge_0 = ns3.NodeContainer()
+    all_bridge_0.Add (term_0)
+    terminalDevices_bridge_0 = ns3.NetDeviceContainer()
+    BridgeDevices_bridge_0 = ns3.NetDeviceContainer()
+    for i in range(1):
+        link = csma_bridge_0.Install(NodeContainer(all_bridge_0.Get(i), bridge_0))
+        terminalDevices_bridge_0.Add(link.Get(0))
+        BridgeDevices_bridge_0.Add(link.Get(1))
+    bridge_bridge_0 = ns3.BridgeHelper
+    bridge_bridge_0.Install(bridge_0.Get(0), BridgeDevices_bridge_0)
+    ndc_bridge_0 = terminalDevices_bridge_0
+    all_bridge_1 = ns3.NodeContainer()
+    all_bridge_1.Add (term_1)
+    terminalDevices_bridge_1 = ns3.NetDeviceContainer()
+    BridgeDevices_bridge_1 = ns3.NetDeviceContainer()
+    for i in range(1):
+        link = csma_bridge_1.Install(NodeContainer(all_bridge_1.Get(i), bridge_1))
+        terminalDevices_bridge_1.Add(link.Get(0))
+        BridgeDevices_bridge_1.Add(link.Get(1))
+    bridge_bridge_1 = ns3.BridgeHelper
+    bridge_bridge_1.Install(bridge_1.Get(0), BridgeDevices_bridge_1)
+    ndc_bridge_1 = terminalDevices_bridge_1
+    all_hub_0 = ns3.NodeContainer()
+    all_hub_0.Add (bridge_0)
+    all_hub_0.Add (bridge_1)
+    ndc_hub_0 = csma_hub_0.Install(all_hub_0)
+
+    # Install the IP stack.
+    internetStackH = ns3.InternetStackHelper()
+    internetStackH.Install (term_0)
+    internetStackH.Install (term_1)
+
+    # IP assign.
+    ipv4 = ns3.Ipv4AddressHelper()
+    ipv4.SetBase (ns3.Ipv4Address("10.0.0.0"), ns3.Ipv4Mask("255.255.255.0"))
+    iface_ndc_bridge_0 = ipv4.Assign (ndc_bridge_0)
+    ipv4.SetBase (ns3.Ipv4Address("10.0.1.0"), ns3.Ipv4Mask("255.255.255.0"))
+    iface_ndc_bridge_1 = ipv4.Assign (ndc_bridge_1)
+    ipv4.SetBase (ns3.Ipv4Address("10.0.2.0"), ns3.Ipv4Mask("255.255.255.0"))
+    iface_ndc_hub_0 = ipv4.Assign (ndc_hub_0)
+
+    # Generate Route.
+    ns3.Ipv4GlobalRoutingHelper.PopulateRoutingTables ()
+
+    # Generate Application.
+    port_tcp_0 = 5500
+    sinkLocalAddress_tcp_0 = ns3.Address(ns3.InetSocketAddress(ns3.Ipv4Address.GetAny(), port_tcp_0))
+    sinkHelper_tcp_0 = ns3.PacketSinkHelper("ns3::TcpSocketFactory", sinkLocalAddress_tcp_0)
+    sinkApp_tcp_0 = sinkHelper_tcp_0.Install(term_1)
+    sinkApp_tcp_0.Start(ns3.Seconds(1.0))
+    sinkApp_tcp_0.Stop(ns3.Seconds(10.0))
+    clientHelper_tcp_0 = ns3.OnOffHelper("ns3::TcpSocketFactory", ns3.Address())
+    clientHelper_tcp_0.SetAttribute("OnTime", ns3.StringValue ("ns3::ConstantRandomVariable[Constant=1]"))
+    clientHelper_tcp_0.SetAttribute("OffTime", ns3.StringValue ("ns3::ConstantRandomVariable[Constant=0]"))
+    clientApps_tcp_0 = ns3.ApplicationContainer()
+    remoteAddress_tcp_0 = ns3.AddressValue(ns3.InetSocketAddress(iface_ndc_bridge_1.GetAddress(0), port_tcp_0))
+    clientHelper_tcp_0.SetAttribute("Remote", remoteAddress_tcp_0)
+    clientApps_tcp_0.Add(clientHelper_tcp_0.Install(term_0))
+    clientApps_tcp_0.Start(ns3.Seconds(1.0))
+    clientApps_tcp_0.Stop(ns3.Seconds(10.0))
+
+    # Simulation.
+    # Pcap output.
+    # Stop the simulation after x seconds.
+    stopTime = 11
+    ns3.Simulator.Stop (ns3.Seconds(stopTime))
+    # Start and clean simulation.
+    ns3.Simulator.Run()
+    ns3.Simulator.Destroy()
+
     framework.stop()
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
